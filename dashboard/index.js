61cb8b8fd2fcb36170ab3ec3faafdb6c866e858d
Chris Wheatley <chriswheatley87@gmail.com>
2015-10-27 13:13:45 +0000

    update import

--- dashboard/index.js
+++ dashboard/index.js
@@ -1,11 +1,11 @@
 import React, {Component} from 'react'
-import {Grid} from '../src/libname'
+import {Grid} from '../src/dashboard'
 
 export default class extends Component {
   render() {
     return (
       <Grid>
-      
+
       </Grid>
     );
   }
