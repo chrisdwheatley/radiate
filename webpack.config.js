0e3edf44fbce7ab5bbf51f25537a8c8afa64288d
Chris Wheatley <chriswheatley87@gmail.com>
2015-10-27 13:03:33 +0000

    basic webpack setup

--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,16 @@ module.exports = {
     path: path.resolve(__dirname, 'build'),
     publicPath: '/static/',
     filename: 'bundle.js'
+  },
+  module: {
+    loaders: [{
+      test: /\.js?$/,
+      loader: 'babel?optional[]=runtime&stage=0',
+      exclude: /node_modules/,
+      includes: [
+        path.join(__dirname, 'src'),
+        path.join(__dirname, 'dashboard')
+      ]
+    }]
   }
 };
