b62464a95642fa6640fda5c1e21bd976ea07fba3
Chris Wheatley <chriswheatley87@gmail.com>
2015-11-04 07:38:44 +0000

    expand upon basic example, add responsiveness

--- dashboard/index.js
+++ dashboard/index.js
@@ -1,27 +1,48 @@
 import React, {Component} from 'react'
-import {Graph, Grid, Item} from '../src'
+import {Dashboard, Graph, Row, Item, Value} from '../src'
+import {optimizedResize, findBreakpoints} from 'react-flexr'
 
 export default class extends Component {
   render () {
     return (
-      <section>
-        <Grid>
-          <Item size='1/2'>
+      <Dashboard>
+        <Row height='m'>
+          <Item width='s'>
+            <Graph data=''/>
+          </Item>
+          <Item width='l' palm='hidden'>
+            <Value freq='1m' name='Branch' />
+          </Item>
+        </Row>
+        <Row height='s'>
+          <Item width='s'>
             <Graph />
           </Item>
-          <Item>
+          <Item width='s'>
             <Graph />
           </Item>
-        </Grid>
-        <Grid>
-          <Item size='1/3'>
+          <Item width='s'>
             <Graph />
           </Item>
-          <Item>
+        </Row>
+        <Row height='l'>
+          <Item width='m'>
             <Graph />
           </Item>
-        </Grid>
-      </section>
+          <Item width='m'>
+            <Graph />
+          </Item>
+        </Row>
+      </Dashboard>
     )
+  }
+
+  constructor() {
+    super()
+    optimizedResize.init(() => {
+      if (findBreakpoints()) {
+        this.forceUpdate()
+      }
+    })
   }
 }
