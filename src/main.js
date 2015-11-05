68d8d622c70f1a214618114a378bbf09ad3bd3d3
Chris Wheatley <chriswheatley87@gmail.com>
2015-11-05 16:37:11 +0000

    add reset css

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Dashboard from '../dashboard'
 import 'react-flexr/styles.css'
+import './reset.css'
 
 ReactDOM.render(
   <Dashboard />,
