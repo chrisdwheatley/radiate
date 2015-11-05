f0a71468ab8ef584d62dd99feb589523d872d3e7
Chris Wheatley <chriswheatley87@gmail.com>
2015-11-05 21:27:03 +0000

    rejig sizings slightly

--- src/cell.js
+++ src/cell.js
@@ -7,12 +7,12 @@ export class Cell extends Component {
       s: {
         palm: '3/6',
         lap: '2/6',
-        desk: '1/6'
+        desk: '2/6'
       },
       m: {
         palm: '6/6',
         lap: '4/6',
-        desk: '2/6'
+        desk: '3/6'
       },
       l: {
         palm: '6/6',
