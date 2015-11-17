a97de23eb3ea724daded83275a8ec442290ceea1
Chris Wheatley <chriswheatley87@gmail.com>
2015-11-17 22:40:41 +0000

    basic polling implementation

--- src/value.js
+++ src/value.js
@@ -1,20 +1,33 @@
 import React, {Component} from 'react'
 import 'whatwg-fetch'
 
+function fetchFile (props) {
+  return fetch(`static/${props.file}`)
+    .then((response) => {
+      return response.json()
+    }).then((json) => {
+      props.onPoll(props.id, json[props.prop])
+    })
+}
+
 export class Value extends Component {
-  constructor(props) {
-    super(props)
-    // fetch(this.props.source).then(res => {
-    //   return res.json()
-    // }).then(data => {
-    //   console.log('parsed data', data[this.props.property])
-    // })
+
+  componentDidMount () {
+    if (this.props.file) {
+      fetchFile(this.props)
+      // switch to setTimeout at some point
+      setInterval(() => {
+        fetchFile(this.props)
+      }, this.props.freq * 1000)
+    }
   }
 
   render () {
+    const {freq, value} = this.props
     return (
-      <div>
-        <h2 style={{padding: '1em', margin: 0, textAlign: 'center'}}>{this.props.title}</h2>
+      <div style={{padding: '1em', margin: 0, textAlign: 'center'}}>
+        <h2>{this.props.title}</h2>
+        <h2>{value[this.props.id] ? value[this.props.id] : ''}</h2>
       </div>
     )
   }
