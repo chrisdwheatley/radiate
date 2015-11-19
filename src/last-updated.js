2eb991bfe6056f7e1be6fce7d0eafeb0beccc37d
Chris Wheatley <chriswheatley87@gmail.com>
2015-11-19 22:19:44 +0000

    basic last updated component

---
+++ src/last-updated.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react'
+
+export class LastUpdated extends Component {
+  render () {
+    return (
+      <span>Last Updated: {new Date().toString()}</span>
+    )
+  }
+}
