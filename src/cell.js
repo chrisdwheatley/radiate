a7e52590ac4f9b13d9cc44da0857daaf088f1115
Chris Wheatley <chriswheatley87@gmail.com>
2015-11-22 10:25:48 +0000

    move out sizes

--- src/cell.js
+++ src/cell.js
@@ -1,39 +1,26 @@
 import React, {Component} from 'react'
 import {Cell as FlexrCell} from 'react-flexr'
+import {SIZES} from './sizes'
+import {LastUpdated} from './last-updated'
 
 export class Cell extends Component {
   render () {
-    const widthsMap = {
-      s: {
-        palm: '3/6',
-        lap: '2/6',
-        desk: '2/6'
-      },
-      m: {
-        palm: '6/6',
-        lap: '4/6',
-        desk: '3/6'
-      },
-      l: {
-        palm: '6/6',
-        lap: '6/6',
-        desk: '4/6'
-      }
-    }
+    const {children, width} = this.props
 
-    const children = React.Children.map(this.props.children, (child) => {
+    const kids = React.Children.map(children, (child) => {
       return React.cloneElement(child, {...this.props})
     })
 
     return (
       <FlexrCell
-        palm={widthsMap[this.props.width].palm}
-        lap={widthsMap[this.props.width].lap}
-        desk={widthsMap[this.props.width].desk}
-        style={{padding: '0', paddingBottom: '22px', transition: 'all .5s ease-in-out'}}>
+        palm={SIZES[width].palm}
+        lap={SIZES[width].lap}
+        desk={SIZES[width].desk}
+        style={{padding: '0', marginBottom: 22, transition: 'all .5s ease-in-out'}}>
           <div
-            style={{background: 'lightgrey', margin: '0 11px', height: '100%'}}>
-            {children}
+            style={{background: '#A490CC', height: '100%', margin: '0 11px', boxShadow: '0 9px 20px rgba(0, 0, 0, 0.3)'}}>
+            {kids}
+            {this.props['last-updated'] ? <LastUpdated /> : null}
           </div>
       </FlexrCell>
     )
