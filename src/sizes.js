d637e81ca850473a65b7d5cf9c0c7c594d1d1169
Chris Wheatley <chriswheatley87@gmail.com>
2015-11-22 10:22:09 +0000

    move sizes out into separate module

---
+++ src/sizes.js
@@ -0,0 +1,17 @@
+export const SIZES = {
+  s: {
+    palm: '3/6',
+    lap: '2/6',
+    desk: '1/6'
+  },
+  m: {
+    palm: '6/6',
+    lap: '4/6',
+    desk: '2/6'
+  },
+  l: {
+    palm: '6/6',
+    lap: '6/6',
+    desk: '4/6'
+  }
+}
