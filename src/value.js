b8406c7d7aef4699fd74cd5bed9fc11354eebb70
Chris Wheatley <chriswheatley87@gmail.com>
2015-11-23 16:31:25 +0000

    move fetch logic into separate module

--- src/value.js
+++ src/value.js
@@ -1,33 +1,32 @@
 import React, {Component} from 'react'
-import 'whatwg-fetch'
-
-function fetchFile (props) {
-  return fetch(`static/${props.file}`)
-    .then((response) => {
-      return response.json()
-    }).then((json) => {
-      props.onPoll(props.id, json[props.prop])
-    })
-}
+import fetch from './fetch'
 
 export class Value extends Component {
 
   componentDidMount () {
-    if (this.props.file) {
-      fetchFile(this.props)
+    const {api, id, file, freq, onPoll, prop} = this.props
+
+    if (file || api) {
+      fetch(file, api).then(res => {
+        onPoll(id, res[prop])
+      })
       // switch to setTimeout at some point
       setInterval(() => {
-        fetchFile(this.props)
-      }, this.props.freq * 1000)
+        fetch(file, api).then(res => {
+          onPoll(id, res[prop])
+        })
+      }, freq * 1000)
     }
+
   }
 
   render () {
-    const {freq, value} = this.props
+    const {freq, id, title, value} = this.props
+
     return (
       <div style={{padding: '1em', margin: 0, textAlign: 'center'}}>
-        <h2>{this.props.title}</h2>
-        <h2>{value[this.props.id] ? value[this.props.id] : ''}</h2>
+        <h3>{title}</h3>
+        <h2>{value[id] ? value[id] : ''}</h2>
       </div>
     )
   }
