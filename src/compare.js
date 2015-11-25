a687deda5c607a96e33394eaa2d23e21088dc82f
Chris Wheatley <chriswheatley87@gmail.com>
2015-11-25 15:51:31 +0000

    basic compare component

---
+++ src/compare.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react'
+
+let prevValue
+
+export class Compare extends Component {
+
+  render () {
+    const {value} = this.props
+    let arrow = <p></p>
+
+    if (value > prevValue) {
+      arrow = <p>/\</p>
+    }
+
+    if (value < prevValue) {
+      arrow = <p>\/</p>
+    }
+
+    prevValue = value
+
+    return arrow
+  }
+}
