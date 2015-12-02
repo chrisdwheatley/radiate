f0695ac76b89aef4a12435563e47f315685ad0e1
Chris Wheatley <chriswheatley87@gmail.com>
2015-12-02 07:32:50 +0000

    allow image to be added locally

--- src/image.js
+++ src/image.js
@@ -1,11 +1,14 @@
 import React, {Component} from 'react'
 
 export class Image extends Component {
-  
+
   render () {
-    const {src} = this.props
+    const {file, source, src} = this.props
+
+    const image = file ? `static/${file}` : src || source
+
     return (
-      <img src={src} style={{minWidth: '50%', maxWidth: '100%'}} />
+      <img src={image} style={{minWidth: '50%', maxWidth: '100%'}} />
     )
   }
 }
