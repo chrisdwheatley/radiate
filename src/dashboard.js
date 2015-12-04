6c8eb32691220a430ad9389aa100d049ff3d4573
Chris Wheatley <chriswheatley87@gmail.com>
2015-12-04 07:30:32 +0000

    use shorthand obj ref

--- src/dashboard.js
+++ src/dashboard.js
@@ -11,12 +11,12 @@ function mapStateToProps (state) {
 class Dashboard extends Component {
 
   render () {
-    const {dispatch} = this.props
+    const {dispatch, value} = this.props
     const children = React.Children.map(this.props.children, (child) => {
       return React.cloneElement(child, {
         onPoll: (id, res) => {
           dispatch(poll(id, res))
-        }, value: this.props.value})
+        }, value})
     })
 
     return (
