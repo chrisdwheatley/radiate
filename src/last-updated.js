e3943e8859d9298dc00149dc83a794a37c95513f
Chris Wheatley <chriswheatley87@gmail.com>
2016-03-26 21:53:25 +0000

    styling for last updated value

--- src/last-updated.js
+++ src/last-updated.js
@@ -1,10 +1,16 @@
 import React, {Component} from 'react'
+import * as moment from 'moment'
+import * as colors from 'material-ui/lib/styles/colors'
 
 export class LastUpdated extends Component {
 
   render () {
+    const now = moment.default()
+    const formattedNow = now.format('MMM Do h:mm:ss a')
     return (
-      <span style={{padding: '1em'}}>Last Updated: {new Date().toString()}</span>
+      <div style={{flex: 1, width: '100%', fontSize: '1.5vw', position: 'absolute', bottom: 0, padding: '1em 0', textAlign: 'center'}}>
+        <span style={{color: colors.pink200}}>Last Updated:</span> {formattedNow}
+      </div>
     )
   }
 }
