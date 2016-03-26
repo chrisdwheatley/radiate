bf8dfcfad3456d7d3f22d0da028706f12aaeaa24
Chris Wheatley <chriswheatley87@gmail.com>
2016-03-26 21:54:22 +0000

    shorthand props

--- src/row.js
+++ src/row.js
@@ -2,13 +2,13 @@ import React, {Component} from 'react'
 import {Grid} from 'react-flexr'
 
 export class Row extends Component {
-  
+
   render () {
+    const {onPoll, value} = this.props
     const children = React.Children.map(this.props.children, (child) => {
-      // not sure why i can't do {...this.props} as the 2nd arg below
       return React.cloneElement(child, {
-        onPoll: this.props.onPoll,
-        value: this.props.value
+        onPoll,
+        value
       })
     })
     return (
