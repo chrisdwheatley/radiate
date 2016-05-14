ee7aa9dcb6806d7a7584a43c4cd0e20765aec3c0
Chris Wheatley <chriswheatley87@gmail.com>
2016-05-14 23:31:47 +0100

    add layout setup for different ergonomics

--- dashboard/index.js
+++ dashboard/index.js
@@ -1,45 +1,31 @@
 import React, {Component} from 'react'
-import {findBreakpoints, optimizedResize} from 'react-flexr'
-import {Cell, Dashboard, Graph, Image, Row, Text, Value} from '../src'
-import injectTapEventPlugin from 'react-tap-event-plugin'
+import {layout} from './layout'
+import {Cell, Dashboard, Graph, Image, Grid, Text, Value} from '../src'
 
 export default class extends Component {
   render () {
     return (
       <Dashboard>
-        <Row>
-          <Cell width='m'>
+        <Grid layout={layout}>
+          <Cell name='title'>
             <Text title='Radiate'>
               A responsive dashboard written in React - built for all screen sizes
             </Text>
           </Cell>
-          <Cell width='s'>
-            <Value title='GitHub Stars:' api='https://api.github.com/repos/swirlycheetah/radiate' prop='stargazers_count' freq={300}/>
-          </Cell>
-        </Row>
-        <Row>
-          <Cell width='s'>
-            <Value title='Share Price:' file='shareprice.json' prop='price' freq={10} prefix='$' compare last-updated/>
-          </Cell>
-          <Cell width='m'>
+          <Cell name='menu'>
             <Value title='Today&#8217;s Special:' file='weeklymenu.json' prop={new Date().getDay()} freq={43200} />
           </Cell>
-        </Row>
-        <Row>
-          <Cell width='l'>
+          <Cell name='shares'>
+            <Value title='Share Price:' file='shareprice.json' prop='price' freq={10} prefix='$' compare last-updated/>
+          </Cell>
+          <Cell name='stars'>
+            <Value title='GitHub Stars:' api='https://api.github.com/repos/swirlycheetah/radiate' prop='stargazers_count' freq={300}/>
+          </Cell>
+          <Cell name='md'>
             <Text title='Supports Markdown' file='text.md' />
           </Cell>
-        </Row>
+        </Grid>
       </Dashboard>
     )
-  }
-
-  constructor (props) {
-    super(props)
-    optimizedResize.init(() => {
-      if (findBreakpoints()) {
-        this.forceUpdate()
-      }
-    })
   }
 }
---
+++ dashboard/layout.js
@@ -0,0 +1,21 @@
+export const layout = {
+  palm: [
+    'title',
+    'menu',
+    'shares',
+    'stars',
+    'md'
+  ],
+  lap: [
+    'title title',
+    'menu shares',
+    'stars md',
+    'stars md'
+  ],
+  desk: [
+    'title title menu',
+    'shares md menu',
+    'stars stars menu',
+    'stars stars menu'
+  ]
+}
