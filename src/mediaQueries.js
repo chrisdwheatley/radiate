34feb10044987796df524d94f5766197de6ecedb
Chris Wheatley <chriswheatley87@gmail.com>
2016-07-27 23:02:26 +0100

    fix(styles): display scrollbar when on mobile size

--- src/mediaQueries.js
+++ src/mediaQueries.js
@@ -8,8 +8,12 @@ export const mediaQueries = {
     fontSize: '4.5vh'
   },
   desk: {
-    size: window.matchMedia('(min-width: 960px)'),
+    size: window.matchMedia('(min-width: 960px) and (max-width: 1279px)'),
     fontSize: '5.5vh'
+  },
+  wall: {
+    size: window.matchMedia('(min-width: 1280px)'),
+    fontSize: '6.5vh'
   }
 }
 
@@ -18,11 +22,18 @@ export function setupMqs(fn) {
     if (mediaQueries[key].size.matches) {
       action(key)
     }
+
+    // display scrollbar if mobile size, otherwise hide it on larger screens
+    if (mediaQueries.palm.size.matches) {
+      document.querySelector('body').style.overflow = 'scroll'
+    } else {
+      document.querySelector('body').style.overflow = 'hidden'
+    }
   }
 
   Object.keys(mediaQueries).forEach(function(key) {
     mediaqueryresponse(key, fn)
-      // need to debounce listener
+    // need to debounce listener
     mediaQueries[key].size.addListener(mediaqueryresponse.bind(null, key, fn))
   })
 }
