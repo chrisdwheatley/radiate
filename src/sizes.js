2831af8cd1163c9c645210689d260f8f9a96d55d
Chris Wheatley <chriswheatley87@gmail.com>
2016-07-27 23:03:34 +0100

    feat(sizes): add wall size

--- src/sizes.js
+++ src/sizes.js
@@ -2,16 +2,19 @@ export const SIZES = {
   s: {
     palm: '3/3',
     lap: '1/3',
-    desk: '1/3'
+    desk: '1/3',
+    wall: '1/3'
   },
   m: {
     palm: '3/3',
     lap: '2/3',
-    desk: '2/3'
+    desk: '2/3',
+    wall: '2/3'
   },
   l: {
     palm: '3/3',
     lap: '3/3',
-    desk: '3/3'
+    desk: '3/3',
+    wall: '3/3'
   }
 }
