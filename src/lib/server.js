aaff7c43ad31c571a57351e72c7879f3d724204e
Chris Wheatley <chriswheatley87@gmail.com>
2016-08-23 21:49:47 +0100

    amend path to built code

--- src/lib/server.js
+++ src/lib/server.js
@@ -5,7 +5,7 @@ const fs = require('fs')
 const path = require('path')
 
 const app = express()
-const staticsPath = __dirname + '/../build/'
+const staticsPath = __dirname + '/../../build/'
 const authCode = process.env.AUTH
 const port = process.env.PORT || 3000
 
