73e16e49139f8972bda62ea7c17edef292c9f6ed
Chris Wheatley <chriswheatley87@gmail.com>
2016-08-27 20:52:48 +0100

    switch from degrees C to F

--- dashboard/index.js
+++ dashboard/index.js
@@ -16,7 +16,7 @@ export default class extends Component {
             <Value title='Data can be fetched, compared & updated from eternal sources, for example Radiate has the following number of GitHub stars' api='https://api.github.com/repos/chrisdwheatley/radiate' prop='stargazers_count' frequency={300} />
           </Cell>
           <Cell name='local-data'>
-            <Value title='Data can be fetched from JSON stored locally and updated via POST, for example the current temperature' file='weather.json' prop='temperature' frequency={10} suffix='°C' compare last-updated/>
+            <Value title='Data can be fetched from JSON stored locally and updated via POST, for example the current temperature' file='weather.json' prop='temperature' frequency={10} suffix='°F' compare last-updated/>
           </Cell>
           <Cell name='getting-started'>
             <Text title='Here are some useful links to help you get started' file='links.md' />
